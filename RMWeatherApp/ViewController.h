8052ed3666deacd191da5c787722c0e392f4a83e
Student P_02 <studentp_02@student-p-02.local>
2016-08-04 17:51:02 +0530

    Initial Commit

---
+++ RMWeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RMWeatherApp
+//
+//  Created by Student P_02 on 04/08/16.
+//  Copyright © 2016 Rahul Mankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RMWeatherApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RMWeatherApp
+//
+//  Created by Student P_02 on 04/08/16.
+//  Copyright © 2016 Rahul Mankar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
